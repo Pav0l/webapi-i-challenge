3a33b1d23f08bd98154d0893c62e7b778aca8515
Pav0l <pavol.karas@gmail.com>
2019-03-18 14:14:38 +0100

    Create GET user by ID endpoint

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const server = express();
 // Parsae the request from client
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+  if (req.body.name && req.body.bio) {
+    db.insert(req.body)
+      .then(newUserId => {
+        res.status(201);
+        res.json({
+          id: newUserId.id,
+          name: req.body.name,
+          bio: req.body.bio
+        });
+      })
+      .catch(err => {
+        res.status(500);
+        res.send({
+        message: "There was an error while saving the user to the database",
+        error: err,
+        });
+      });
+  } else {
+    res.status(400);
+    res.send({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
+
 server.get('/api/users', (req, res) => {
   db.find()
     .then(users => res.json(users))
@@ -19,23 +43,25 @@ server.get('/api/users', (req, res) => {
     });
 });
 
-server.post('/api/users', (req, res) => {
-  if (req.body.name && req.body.bio) {
-    db.insert(req.body)
-      .then(newUserId => {
-        res.json(newUserId)
-      })
-      .catch(err => ({
-        message: "There was an error while saving the user to the database",
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(users => {
+      if (!users) {
+        res.status(404);
+        res.json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.json(users);
+      }
+    })
+    .catch(err => {
+      res.status(500);
+      res.send({
+        message: "The user information could not be retrieved.",
         error: err,
-      }));
-  } else {
-    res.status(400);
-    res.send({ errorMessage: "Please provide name and bio for the user." })
-  }
+      });
+    });
 });
-
-
 
 
 
