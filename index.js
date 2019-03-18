1bfedaeb056a818fc575a48c9a5e7194a98853a4
Pav0l <pavol.karas@gmail.com>
2019-03-18 13:29:57 +0100

    Create GET request for users

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const db = require('./data/db');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hello from Express');
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => res.json(users))
+    .catch(err => {
+      res.status(500);
+      res.send({
+        error: "There was an error while saving the user to the database",
+        message: err,
+      });
+    });
 });
+
+
+
+
 
 server.listen(5000, () =>
   console.log('Server running on http://localhost:5000')
